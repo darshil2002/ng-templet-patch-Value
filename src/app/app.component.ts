316b4a9ff5ae477f527551328fc607d2cef64be4
darshil2002 <97335353+darshil2002@users.noreply.github.com>
2023-04-11 17:24:26 +0530

    ng-templet | patchValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {FormGroup,FormGroupName,FormControl} from '@angular/forms'
+import Chart, { registerables } from 'chart.js/auto';
+
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('myChart') myChart!: ElementRef;
+  x:boolean=true;
+
+  
+  y=[
+    {name:'darshil',age:10},
+    {name:'darsh', age: 26}
+  ]
+
+  myForm=new FormGroup({
+    user: new FormControl(''),
+    age : new FormControl(''),
+  })
+  
+  ngOnInit(): void {
+  }
+
+  sub(){
+    var us=this.myForm.get('user')?.value;
+    var ag=this.myForm.get('age')?.value;
+    console.log('my name is '+ us +'and age is '+ ag);
+  }
+  reserFunc(){
+    this.myForm.reset()
+  }
+  fillValue(){
+    this.myForm.patchValue({
+      "user":'darsh',
+      "age": '12',
+    })
+  }
+  
   title = 'ngTempOne';
 }
